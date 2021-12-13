68298dce7f36432cc7b8b4044f0e72738629a44a
lnannettik <l.nannetti.k@gmail.com>
2021-12-13 15:42:49 +0100

    finished secC | missing color

--- src/components/SectionC.vue
+++ src/components/SectionC.vue
@@ -1,6 +1,29 @@
 <template>
-<h1>ojncvnwijcnjkwnc</h1>
-  
+    <section class="sectionC">
+
+        <div class="sec-container d-flex offset-2 col-8">
+            <div class="img-container col-2">
+                <img class="less" src="../assets/logo-5.png" alt="less">
+            </div>
+            <div class="img-container col-2">
+                <img class="center" src="../assets/logo-1.png" alt="less">
+            </div>
+            <div class="img-container col-2">
+                <img class="center" src="../assets/logo-2.png" alt="less">
+            </div>
+            <div class="img-container col-2">
+                <img class="center" src="../assets/logo-3.png" alt="less">
+            </div>
+            <div class="img-container col-2">
+                <img class="center" src="../assets/logo-4.png" alt="less">
+            </div>
+            <div class="img-container col-2 less">
+                <img class="less" src="../assets/logo-5.png" alt="less">
+            </div>
+        </div>
+
+    
+    </section>  
 </template>
 
 <script>
@@ -11,5 +34,32 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+    .sectionC {
+        background: #eef4ed;
+        height: 280px;
+        color: #737373;
+
+        
+
+        .img-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            color: #737373;
+
+
+            .less {
+                color: #737373;
+                width: 2.5rem;
+            }
+
+            .center {
+                width: 6rem;
+            }
+
+        }
+    }
 
 </style>
